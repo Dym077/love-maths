8d0c39b080513b2299dfcc1c839928821ae1a683
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2023-12-12 14:55:36 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,49 @@
+// Wait for the DOM to finish loading this game
+// Get the button elements and add event listeners
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
 
 }
 
 function checkAnswer() {
 
-} 
+}
 
 function calculateCorrectAnswer() {
 
 }
 
-function incrementScore () {
+function incrementScore() {
 
 }
 
-function incrementWrongAnswer () {
+function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion () {
+function displayAdditionQuestion() {
 
 }
 
-function displaySubtractQuestion () {
+function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion () {
-    
+function displayMultiplyQuestion() {
+
 }
